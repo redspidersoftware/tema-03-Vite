48ab09e3e8d32fffcd403fe3c6ff381b1e42cb9d
JavierGonzalez <ochentainueve@gmail.com>
2023-06-20 16:49:07 +0200

    seguimos con vite

---
+++ src/Main.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        Helo world
+        <input type = "text" v-model="valorInput" />
+        <button @click="pasarPorConsola"> Pasar por consola</button>
+        <p>{{ valorMayusculas }}</p>
+    </div>
+  </template>
+  
+  <script setup lang="ts">
+import {ref, computed} from "vue"
+const valorInput = ref("Añada la sal a la salsa")
+
+const pasarPorConsola = () =>{
+    console.log(valorInput.value)
+}
+
+const valorMayusculas = computed(()=>{
+    return valorInput.value.toUpperCase()
+})
+
+  </script>
+  <style scoped>
+  
+  </style>
+  
