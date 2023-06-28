21a58ee3e4ecd7dace4de903cc5d278020490ad3
JavierGonzalez <ochentainueve@gmail.com>
2023-06-28 15:07:39 +0200

    apis con axios

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,102 @@
 <template>
-  <nav class="navbar">
-    <router-link to="/" active-class="link-activo">Main</router-link>
-    <router-link to="/contador" active-class="link-activo">Contador</router-link>
-    <router-link to="/usuarios" active-class="link-activo">Usuarios</router-link>
-    <router-link to="/articulo" active-class="link-activo">Artículo</router-link>
-    <router-link :to="{ path: '/articulo', hash: '#segundaparte' }" active-class="link-activo">Segundo</router-link>
-    <router-link :to="{ path: '/prohibido' }">LO PROHIBIDO</router-link>
+    <h1>PokeApi</h1>
+    <h3>Introduce el id del pokemon que quieres buscar</h3>
+    <input type="number" v-model="id" placeholder="Introduce el id" />
+    <button @click="buscaPokemonAxios">Buscar Axios</button>
+    <button @click="buscaPokemonFetch">Buscar Fetch</button>    
+    <div v-if="pokemon">
+      <h3>{{ pokemon.id }}. {{ pokemon.name }}</h3>
+      <img :src="pokemon.sprites.front_default" :alt="pokemon.name" />
+    </div>
+    <br/>
+    <input type="text" v-model="nameCarta" placeholder="nombre de la carta" />
+    <button @click="buscaHearthsone">Buscar Hearthsone</button>
+    <div v-if="carta">
+      <h3>{{ carta[0].cardId }}. {{ carta[0].name }}</h3>
+       <img :src="carta[3].img" :alt="carta[0].name" /> 
+    </div>
+  </template>
+  
+  <script setup lang="ts">
+  import { ref } from "vue";
+  import axios from "axios";
+  import { pokeAxios } from "./assets/axiosInstances"
+  
+  interface Pokemon {
+    id: number;
+    name: string;
+    sprites: {
+      front_default: string;
+    };
+  }
+  
+  const id = ref("");
+  const nameCarta = ref("");
+  const pokemon = ref<Pokemon | null>(null);
+  const carta = ref("") 
+  
+  const buscaPokemonFetch = async () => {
+    // Hacer la llamada a la API
+  
+    // Utilizando FETCH
+    const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${id.value}`)
+      .catch((error) => console.error(error));
+    
+    if (!res) return
+  
+    const data = await res.json().catch((error) => console.error(error));
+    console.log(data);
+    pokemon.value = data;
+  };
+  const buscaPokemonAxios = async () => {
+    // Hacer la llamada a la API
+  
+    // Utilizando AXIOS
+    // const res = await axios
+    //   .get(`https://pokeapi.co/api/v2/pokemon/${id.value}`, { 
+    //     transformResponse(res) { // Interceptor de respuesta
+    //       console.log(JSON.parse(res))
+    //       return {
+    //         ...JSON.parse(res),
+    //         successful: true
+    //       }
+    //     },
+    //     transformRequest(req) { // Interceptor de solicitud
+    //       console.log(req)
+    //       return req
+    //     }
+    //   })
+    //   .catch((error) => console.error(error));
+  
+    const res = await pokeAxios.get(id.value.toString())
+  
+    if (!res) return
+  
+    console.log(res.data);
+    pokemon.value = res.data;
+  };
 
+  const buscaHearthsone = async () =>{
+    
 
-  </nav>
-  <router-view v-slot="{ Component }">
-    <transition name="fade" mode="out-in">
-      <component :is="Component" />
-    </transition>
-  </router-view>
-</template>
+const options = {
+  method: 'GET',
+  url: `https://omgvamp-hearthstone-v1.p.rapidapi.com/cards/${nameCarta.value}`,
+  headers: {
+    'X-RapidAPI-Key': 'QcZUPWEFrsmshDUXqDnj1bZLPseNp1cm5cYjsnQ2zFFC7f9H9T',
+    'X-RapidAPI-Host': 'omgvamp-hearthstone-v1.p.rapidapi.com'
+  }
+};
 
-<script setup lang="ts">
-</script>
-<style scoped>
-.link-activo {
-    text-transform: uppercase;
-    text-shadow: 0 0 2rem rgb(136, 255, 125);
+try {
+	const response = await axios.request(options);
+    carta.value = response.data
+	console.log(response.data);
+} catch (error) {
+	console.error(error);
 }
-
-.navbar {
-  width: 100%;
-  background-color: rgba(140, 140, 140, 0.5);
-  padding: 1rem;
-  display: flex;
-  gap: 1rem;
-}
-.fade-enter-active, .fade-leave-active {
-  transition: all 0.3s;
-}
-.fade-enter, .fade-leave-to {
-  opacity: 0;
-  transform: scale(0.9);
-}
-</style>
+  }
+  </script>
+  
+  <style scoped></style>
+  
---
+++ src/assets/axiosInstances.ts
@@ -0,0 +1,11 @@
+import axios from "axios"
+
+const token = "1231819181fda1sdf18as1fw3f5s"
+
+export const pokeAxios = axios.create({
+    baseURL: "https://pokeapi.co/api/v2/pokemon/",
+    headers: {
+        Authorization: `Bearer ${token}`,
+        "Content-Type": "application/json"
+    }
+})
